61ae48c33d12e62f1ab15886ff0e9ae921b176dd
relays <S1510239012@students.fh-hagenberg.at>
2017-07-31 17:59:27 +0200

    added mode of operation

--- miniaes_tutorial.py
+++ miniaes_tutorial.py
@@ -9,23 +9,32 @@ Disclaimer:
 
 
 def start(): 
-    #TODO
 #offer to tell people about nibbles. 
-    input('For the following, please type yes or no. Would you like to hear about nibbles, which are the components of miniaes?')
+    putty=input('For the following, please type yes or no. Would you like to hear about nibbles, which are the components of miniaes?')
     label: backnibble
-    if input=='yes': 
-        input('A nibble\'s size is four bits. It can be thought as a finite field. Dou you know what a finite field is? ')
-        if input=='no': 
+    if putty=='yes': 
+        putty=input('A nibble\'s size is four bits. It can be thought as a finite field. Dou you know what a finite field is? ')
+        if putty=='no': 
             print('A finite field is is an area of numbers where the operations +, -, * and / are all possible. If you perform them, you will stay in the field.' + 
                     'It can be expressed as a polynomial. For example, the nibble n=1010 would be the polynomial n=1*x^3+0*x^2+1*x^1+0*x^0')
-            input('Would you like to hear more about fields?')
-            if input=='yes':
+            putty=input('Would you like to hear more about fields?')
+            if putty=='yes':
                 print('Fields are a very thouroghly researched subject in maths. I would refer you the paper this program is based on. ' + 
                         'You can find it here: http://staff.guilan.ac.ir/staff/users/rebrahimi/fckeditor_repo/file/mini-aes-spec.pdf')
-    elif input=='no':
+    elif putty=='no':
         continue
     else: 
-        input('Sorry, I did not understand. Would you like to hear about nibbles? Type yes or no')
+        putty=input('Sorry, I did not understand. Would you like to hear about nibbles? Type yes or no')
         goto backnibble
+
+#modes of operation
+    putty=input('Would you like to know how the ciphertext is generated from the plaintext?')
+    if putty=='yes': 
+        print('AES has so-called Modes of Operation, who determine how the ciphertext is generated from the plaintext. ' +
+            'Mini-AES does not. It just encrypts block by block with the key, without taking the result of the previous block in account. This is equivalent to ' + #TODO
+            'in AES. ')
+
+
+
 
 start()
