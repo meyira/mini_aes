f07da108016dc75280d7d6f7f5a6c288ae3c8123
Lena Heimberger <S1510239012@students.fh-hagenberg.at>
2018-01-14 20:01:47 +0100

    fixed error (first nibble was never included)

--- miniaes_functions.py
+++ miniaes_functions.py
@@ -1,3 +1,4 @@
+import binascii
 nibble_size=4 #a nibble is four bits. to ease the calculation, it will be four elements in an array (2-dimensional)
 
 def string_to_binary(string):
@@ -5,7 +6,8 @@ def string_to_binary(string):
     arg0: the string that should be converted to a binary file
     return: the string in binary representation
     """
-    binary_rep = ''.join(format(ord(x), 'b') for x in string)
+    binary_rep = ' '.join('{0:08b}'.format(ord(x), 'b') for x in string)
+    print(binary_rep)
     return binary_rep
 
 def generate_matrix(binary):
